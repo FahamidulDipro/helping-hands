7a0032f60432b70b7ec91f49c2cf38994111e6b7
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-27 03:43:21 +0600

    Selected services are shown in Events route

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Button, Col, Figure, Row } from "react-bootstrap";
 
 const Events = () => {
-    return (
-        <div className='mt-5'>
-            <h1>Events</h1>
-        </div>
-    );
+  const [selectedServices, setSelectedServices] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/selected-services")
+      .then((res) => res.json())
+      .then((data) => setSelectedServices(data));
+  }, []);
+  return (
+    <div className="mt-5">
+      <h1>Selected Events</h1>
+      <Row>
+        {selectedServices.map((selectedService) => (
+          <Col
+            lg={6}
+            key={selectedService._id}
+            className="d-flex justify-content-center"
+          >
+            <Figure className="d-flex justify-content-between border p-3 w-75 position-relative">
+              <div className="d-flex  w-75">
+                <Figure.Image
+                  width={171}
+                  height={180}
+                  alt="171x180"
+                  src={selectedService.img}
+                />
+                0
+                <Figure.Caption className="ms-3">
+                  <h2>{selectedService.name}</h2>
+                </Figure.Caption>
+              </div>
+              <div>
+                <Button className="float-end position-absolute bottom-0 end-0 m-3 bg-secondary">
+                  Cancel
+                </Button>
+              </div>
+            </Figure>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default Events;
--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Button, Card, Col } from 'react-bootstrap';
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
 
-const Service = ({service}) => {
-    const {name,img,detail} = service;
-    return (
-        <Col lg={4} className='g-5'>
-          <Card style={{ width: '18rem' ,borderRadius:'20px'}}>
-        <Card.Img variant="top" src={img} style={{borderTopLeftRadius:'20px',borderTopRightRadius:'20px'}}/>
+const Service = ({ service }) => {
+  const { _id, name, img } = service;
+  const selectService = (id) => {
+    fetch("http://localhost:5000/services", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(service),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+  };
+  return (
+    <Col lg={4} className="g-5">
+      <Card
+        style={{ width: "18rem", borderRadius: "20px" }}
+        onClick={() => selectService(_id)}
+      >
+        <Card.Img
+          variant="top"
+          src={img}
+          style={{ borderTopLeftRadius: "20px", borderTopRightRadius: "20px" }}
+        />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
-       
-       
         </Card.Body>
-      </Card></Col>
-      
-    );
+      </Card>
+    </Col>
+  );
 };
 
 export default Service;
